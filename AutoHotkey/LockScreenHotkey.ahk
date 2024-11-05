96bc8253b8b16832549464f1901f45c8346d1bdc
alexanderhancock-dev <alexanderhancock07@gmail.com>
2024-11-05 12:29:09 +0000

    Added initial scripts and configs

---
+++ AutoHotkey/LockScreenHotkey.ahk
@@ -0,0 +1,9 @@
+﻿#Requires AutoHotkey v2.0
+
+#HotIf GetKeyState("F22")
+z up::
+{
+Run('schtasks /Run /TN LockEnable')
+Run('cmd /c "Rundll32.exe user32.dll,LockWorkStation"')
+}
+#HotIf
---
+++ install-personal.bat
@@ -0,0 +1,17 @@
+winget install glzr-io.glazewm
+winget install glzr-io.zebar
+winget install eloston.ungoogled-chromium
+winget install Discord.Discord
+winget install Valve.Steam
+winget install Microsoft.VisualStudio.2022.Community
+winget install MultiMC.MultiMC
+winget install voidtools.Everything
+winget install WinDirStat.WinDirStat
+winget install Git.Git
+winget install Microsoft.PowerToys
+winget install Cygwin.Cygwin
+:: Apple Music
+winget install 9PFHDD62MXS1
+winget install OBSProject.OBSStudio
+winget install AutoHotkey.AutoHotkey
+winget install Nvidia.GeForceExperience
