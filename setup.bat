198868e51f659654bd6e15ab1b62b7220f0c19ac
ahan-dev <alexanderhancock07@gmail.com>
2024-11-20 19:16:36 +1100

    Get komorebi to fetch applications.json

--- setup.bat
+++ setup.bat
@@ -4,3 +4,4 @@ schtasks /create /xml Tasks\LockDisable /tn LockDisable /f
 schtasks /create /xml Tasks\LockEnable /tn LockEnable /f
 FOR /F %%I IN ('dir %USERPROFILE%\AppData\Local\Packages\Microsoft.WindowsTerminal* /B') DO @SET "termpath=%%I"
 copy wt\settings.json %USERPROFILE%\AppData\Local\Packages\%termpath%\LocalState\
+komorebic fetch-asc
